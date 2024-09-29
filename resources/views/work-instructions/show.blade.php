c6a1c94c2a21889d9425a9cc5cddbb597bfba8f9
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-09-29 23:39:19 +0700

    update: persist user_id when editing work instruction

--- resources/views/work-instructions/show.blade.php
+++ resources/views/work-instructions/show.blade.php
@@ -33,14 +33,13 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="card card-info card-outline card-outline-tabs">
-
                                     <div class="card-body">
                                         <h6 class="text-divider mb-4"><span>Edit Work Instructions</span></h6>
                                         <div class="form-group row">
                                             <label class="col-sm-2 col-form-label text-right">Worker</label>
                                             <div class="col-sm-4">
                                                 <select class="form-control form-control-sm select2" name="user_id" disabled>
-                                                    <option value="">-- Pilih Worker --</option>
+                                                    <option value="{{$workInstruction->user->id}}" selected>{{$workInstruction->user->name}}</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -87,45 +86,45 @@
 
 
 @section('scripts')
-    <script>
-        $(document).ready(function() {
-            const loggedInUser = {
-                id: '{{ $workInstruction->user_id }}',
-                text: '{{ $workInstruction->user->name }}',
-            };
+{{--    <script>--}}
+{{--        $(document).ready(function() {--}}
+{{--            const loggedInUser = {--}}
+{{--                id: '{{ $workInstruction->user_id }}',--}}
+{{--                text: '{{ $workInstruction->user->name }}',--}}
+{{--            };--}}
 
-            $('.select2').select2({
-                placeholder: '-- Pilih Worker --',
-                allowClear: true,
-                ajax: {
-                    url: '{{ route('users.index') }}',
-                    dataType: 'json',
-                    delay: 250,
-                    data: function(params) {
-                        return {
-                            q: params.term, // search term
-                            intent: '{{ IntentEnum::USER_SEARCH_USERS->value }}', // intent parameter
-                        };
-                    },
-                    processResults: function(data) {
-                        return {
-                            results: data.map(function(user) {
-                                return {
-                                    id: user.id,
-                                    text: `${user.nip} - ${user.name}`,
-                                };
-                            }),
-                        };
-                    },
-                    cache: true,
-                },
-                initSelection: function(element, callback) {
-                    callback(loggedInUser);
-                },
-            });
+{{--            $('.select2').select2({--}}
+{{--                placeholder: '-- Pilih Worker --',--}}
+{{--                allowClear: true,--}}
+{{--                ajax: {--}}
+{{--                    url: '{{ route('users.index') }}',--}}
+{{--                    dataType: 'json',--}}
+{{--                    delay: 250,--}}
+{{--                    data: function(params) {--}}
+{{--                        return {--}}
+{{--                            q: params.term, // search term--}}
+{{--                            intent: '{{ IntentEnum::USER_SEARCH_USERS->value }}', // intent parameter--}}
+{{--                        };--}}
+{{--                    },--}}
+{{--                    processResults: function(data) {--}}
+{{--                        return {--}}
+{{--                            results: data.map(function(user) {--}}
+{{--                                return {--}}
+{{--                                    id: user.id,--}}
+{{--                                    text: `${user.nip} - ${user.name}`,--}}
+{{--                                };--}}
+{{--                            }),--}}
+{{--                        };--}}
+{{--                    },--}}
+{{--                    cache: true,--}}
+{{--                },--}}
+{{--                initSelection: function(element, callback) {--}}
+{{--                    callback(loggedInUser);--}}
+{{--                },--}}
+{{--            });--}}
 
-            // Set the value and trigger change to select the logged-in user
-            $('.select2').val(loggedInUser.id).trigger('change');
-        });
-    </script>
+{{--            // Set the value and trigger change to select the logged-in user--}}
+{{--            $('.select2').val(loggedInUser.id).trigger('change');--}}
+{{--        });--}}
+{{--    </script>--}}
 @endsection
