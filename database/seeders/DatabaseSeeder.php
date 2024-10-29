a6e692dac5559b461c64cc230ceb0f26cb678c7a
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-10-30 03:36:11 +0700

    update: add seeder condition

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,36 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
+    private bool $isProduction = true;
+
     /**
      * Seed the application's database.
      */
     public function run(): void {
+        User::create([
+            'nip' => '1',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
+
         $this->call([
             UnitSeeder::class,
-            UserSeeder::class,
             MenuSeeder::class,
             AccessMenuSeeder::class,
+        ]);
+
+        if ($this->isProduction) {
+            return;
+        }
+
+        $this->call([
+            UserSeeder::class,
             WorkInstructionSeeder::class,
             AssignmentSeeder::class,
             PCSeeder::class,
