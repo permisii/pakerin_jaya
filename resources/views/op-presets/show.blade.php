5339f918dd7bcf4eb891d983bb95f568e6f7df55
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-12-03 22:03:05 +0700

    update: rename op and op-preset field names

--- resources/views/op-presets/show.blade.php
+++ resources/views/op-presets/show.blade.php
@@ -34,7 +34,7 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label class="col-sm-2 col-form-label text-right">Kode</label>
+                                    <label class="col-sm-2 col-form-label text-right">Kode Kasi</label>
                                     <div class="col-sm-4">
                                         <input type="text" class="form-control form-control form-control-sm"
                                                value="{{ $opPreset->code }}" name="code">
@@ -58,7 +58,7 @@
                                 {{--                                </div>--}}
 
                                 <div class="form-group row">
-                                    <label class="col-sm-2 col-form-label text-right">Peminta 1</label>
+                                    <label class="col-sm-2 col-form-label text-right">Divisi Head</label>
                                     <div class="col-sm-4">
                                         <input type="text" class="form-control form-control form-control-sm"
                                                value="{{ $opPreset->first_requestor }}" name="first_requestor">
@@ -66,7 +66,7 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label class="col-sm-2 col-form-label text-right">Peminta 2</label>
+                                    <label class="col-sm-2 col-form-label text-right">Plan Manager</label>
                                     <div class="col-sm-4">
                                         <input type="text" class="form-control form-control form-control-sm"
                                                value="{{ $opPreset->second_requestor }}" name="second_requestor">
