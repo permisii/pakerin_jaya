1af3d325d2e60ba707dd538996d4d07c3b7e9f13
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-12-05 00:37:38 +0700

    experimental: show op using flex layout

--- resources/views/ops/show.blade.php
+++ resources/views/ops/show.blade.php
@@ -5,115 +5,156 @@
 @section('content')
     <div class="container mt-5">
         <div class="card p-4">
-            <table id="printableTable" style="width: 236.97mm; border-collapse: collapse;">
-                <thead>
-                <tr>
-                    <th colspan="2"
-                        style="width: 63.23mm; height: 4.33mm; text-align: center; border: 1px solid black;">COST CENTER
-                    </th>
-                    <th colspan="5" rowspan="3"
-                        style="width: 70.67mm; height: 17.44mm; text-align: center; border: 1px solid black;">ORDER
+            <div class="flex-container" id="printableArea"
+                 style="display: flex; flex-direction: column; width: 195.39mm; font-size: 12px; border: 1px solid black">
+
+                <div class="header-row" style="display: flex">
+                    <div class="d-flex flex-column" style="width: 63.23mm; height: 17.44mm;">
+                        <p class="m-0"
+                           style="height: 4.33mm; text-align: center; border-right: 1px solid black; border-bottom: 1px solid black">
+                            COST
+                            CENTER</p>
+                        <div class="d-flex"
+                             style="height: 6.56mm;border-right: 1px solid black; border-bottom: 1px solid black">
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 16.42mm">NAMA :</p>
+                            <p class="m-0 ml-2 d-flex align-items-center"
+                               style="width: 45.07mm;">{{$op->department}}</p>
+                        </div>
+                        <div class="d-flex"
+                             style="height: 6.56mm;border-right: 1px solid black; border-bottom: 1px solid black">
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 16.42mm">KODE :</p>
+                            <p class="m-0 ml-2 d-flex align-items-center"
+                               style="width: 45.07mm;">{{$op->code}}</p>
+                        </div>
+                    </div>
+                    <p style="display: flex; align-items: center; justify-content: center; width: 70.67mm; text-align: center; padding: 0;margin: 0; border-bottom: 1px solid black">
+                        ORDER
                         PEMBELIAN
-                    </th>
+                    </p>
+                    <div class="d-flex flex-column" style="width: 61.49mm; height: 17.44mm;">
+                        <div class="d-flex" style="height: 8.74mm;">
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 16.42mm; border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black">
+                                NO.
+                                :</p>
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 45.07mm; border-bottom: 1px solid black; ">{{$op->no}}</p>
+                        </div>
+                        <div class="d-flex" style="height: 8.74mm;">
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 16.42mm; border-left: 1px solid black; border-right: 1px solid black; border-bottom: 1px solid black; ">
+                                TGL. :</p>
+                            <p class="m-0 d-flex align-items-center"
+                               style="width: 45.07mm; border-bottom: 1px solid black; ">
+                                @if($op->isValidDate())
+                                    {{(new DateTime($op->date_needed))->format('d F Y')}}
+                                @else
+                                    {{$op->date_needed}}
+                                @endif
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
-                    <th rowspan="2" style="width: 16.42mm; height: 8.74mm; border: 1px solid black;">NO:</th>
-                    <th rowspan="2" style="width: 45.07mm; height: 8.74mm; border: 1px solid black;">{{$op->no}}</th>
-                </tr>
-                <tr>
-                    <th style="width: 16.42mm; height: 6.56mm; border: 1px solid black;">NAMA:</th>
-                    <th style="width: 45.07mm; height: 6.56mm; border: 1px solid black;">{{$op->department}}</th>
+                <div class="header-row"
+                     style="display: flex; border-top: 1px solid black; border-bottom: 1px solid black; height: 12.77mm; margin-top: 2px;">
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 10.60mm; border-right: 1px solid black; text-align: center;">NO.
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 46.32mm; border-right: 1px solid black; text-align: center;">NAMA BARANG
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 21.81mm; border-right: 1px solid black; text-align: center;">INDEX
+                    </div>
+                    <div class="d-flex" style="width: 35.75mm; text-align: center; border-right: 1px solid black;">
+                        <div class="d-flex flex-fill flex-column">
+                            <p class="m-0 " style="border-bottom: 1px solid black;">KWANTUM</p>
+                            <div class="d-flex justify-content-between flex-fill w-100 h-100">
+                                <div
+                                    class="d-flex align-items-center justify-content-center"
+                                    style="height: 100%; width: 11.44mm; border-right: 1px solid black; text-align: center">
+                                    SISA
+                                </div>
+                                <div
+                                    class="d-flex align-items-center justify-content-center"
+                                    style="height: 100%; width: 11.44mm; border-right: 1px solid black; text-align: center">
+                                    PERLU
+                                </div>
+                                <div
+                                    class="d-flex align-items-center justify-content-center"
+                                    style="height: 100%; width: 11.44mm; text-align: center">
+                                    BELI
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 11.13mm; border-right: 1px solid black; text-align: center;">UNIT
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 14.75mm; border-right: 1px solid black; text-align: center;">TGL. BUTUH
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center"
+                         style="width: 56.22mm; text-align: center;">KETERANGAN
+                    </div>
+                    <div style="flex: 1;"></div>
+                </div>
 
-                </tr>
-                <tr>
-                    <th style="width: 16.42mm; height: 6.56mm; border: 1px solid black;">KODE:</th>
-                    <th style="width: 45.07mm; height: 6.56mm; border: 1px solid black;">{{$op->code}}</th>
-                    <th style="width: 16.42mm; height: 8.74mm; border: 1px solid black;">TGL:</th>
-                    <th style="width: 45.07mm; height: 8.74mm; border: 1px solid black;">
-                        @if($op->isValidDate())
-                            {{(new DateTime($op->date_needed))->format('d F Y')}}
-                        @else
-                            {{$op->date_needed}}
-                        @endif
-                    </th>
-                </tr>
-                <tr>
-                    <th rowspan="2"
-                        style="width: 10.60mm; height: 12.77mm; border: 1px solid black; text-align: center;">NO.
-                    </th>
-                    <th rowspan="2"
-                        style="width: 46.32mm; height: 12.77mm; border: 1px solid black; text-align: center;">NAMA
-                        BARANG
-                    </th>
-                    <th rowspan="2"
-                        style="width: 21.81mm; height: 12.77mm; border: 1px solid black; text-align: center;">INDEX
-                    </th>
-                    <th colspan="3"
-                        style="width: 35.75mm; height: 6.67mm; text-align: center; border: 1px solid black;">KWANTUM
-                    </th>
-                    <th rowspan="2"
-                        style="width: 11.33mm; height: 12.77mm; border: 1px solid black; text-align: center;">UNIT
-                    </th>
-                    <th rowspan="2"
-                        style="width: 14.75mm; height: 12.77mm; border: 1px solid black; text-align: center;">TGL. BUTUH
-                    </th>
-                    <th rowspan="2"
-                        style="width: 56.22mm; height: 12.77mm; border: 1px solid black; text-align: center;">KETERANGAN
-                    </th>
-                </tr>
-                <tr>
-                    <th style="width: 11.44mm; height: 6.67mm; border: 1px solid black; text-align: center;">SISA</th>
-                    <th style="width: 11.44mm; height: 6.67mm; border: 1px solid black; text-align: center;">PERLU</th>
-                    <th style="width: 11.44mm; height: 6.67mm; border: 1px solid black; text-align: center;">BELI</th>
-                </tr>
-                </thead>
-                <tbody>
                 @foreach($op->pps as $pp)
-                    <tr>
-                        <td style="width: 10.60mm; height: 5.77mm; border: 1px solid black;">{{$loop->iteration}}</td>
-                        <td style="width: 46.32mm; height: 5.77mm; text-align: left; border: 1px solid black;">{{$pp->item_name}}</td>
-                        <td style="width: 21.81mm; height: 5.77mm; border: 1px solid black;"></td>
-                        <td style="width: 11.44mm; height: 5.77mm; border: 1px solid black;">{{$pp->remaining}}</td>
-                        <td style="width: 11.44mm; height: 5.77mm; border: 1px solid black;">{{$pp->need}}</td>
-                        <td style="width: 11.44mm; height: 5.77mm; border: 1px solid black;">{{$pp->buy}}</td>
-                        <td style="width: 11.33mm; height: 5.77mm; border: 1px solid black;">{{$pp->unit}}</td>
-                        <td style="width: 14.75mm; height: 5.77mm; border: 1px solid black;">{{$pp->need_date->format('d-m-y')}}</td>
-                        <td style="width: 56.22mm; height: 5.77mm; text-align: left; border: 1px solid black;">{{$pp->description}}</td>
-                    </tr>
+                    {{--                    remove the height so the value can adapt, but who knows --}}
+
+                    <div class="data-row"
+                         style="display: flex; height: 5.77mm; border-bottom: 1px solid black;">
+                        {{--                        If height is removed, feel free to remove overflow --}}
+                        <div
+                            style="width: 10.60mm; border-right: 1px solid black; overflow: hidden; text-align: center">{{$loop->iteration}}</div>
+                        <div
+                            style="width: 46.32mm; text-align: left; border-right: 1px solid black; overflow: hidden">{{$pp->item_name}}</div>
+                        <div style="width: 21.81mm; border-right: 1px solid black; overflow: hidden"></div>
+                        <div
+                            style="width: 11.44mm; border-right: 1px solid black; overflow: hidden">{{$pp->remaining}}</div>
+                        <div style="width: 11.44mm; border-right: 1px solid black; overflow: hidden">{{$pp->need}}</div>
+                        <div style="width: 11.44mm; border-right: 1px solid black; overflow: hidden">{{$pp->buy}}</div>
+                        <div style="width: 11.13mm; border-right: 1px solid black; overflow: hidden">{{$pp->unit}}</div>
+                        <div
+                            style="width: 14.75mm; border-right: 1px solid black; overflow: hidden">{{$pp->need_date->format('d-m-y')}}</div>
+                        <div
+                            style="width: 56.22mm; text-align: left; overflow: hidden">{{$pp->description}}</div>
+                        <div style="flex: 1;"></div>
+                    </div>
                 @endforeach
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td colspan="2"
-                        style="width: 103.07mm; height: 21.78mm; border: 1px solid black; vertical-align: bottom">
+
+                <div class="footer-row" style="display: flex; height: 21.78mm">
+                    <div
+                        style="width: 103.07mm; border-right: 1px solid black; vertical-align: bottom; display: flex; flex: 1; justify-content: space-between; align-items: end">
                         <p class="m-0 mb-1">{{$op->first_requestor}}</p>
-                    </td>
-                    <td colspan="2"
-                        style="width: 103.07mm; height: 21.78mm; border: 1px solid black; vertical-align: bottom">
                         <p class="m-0 mb-1">{{$op->second_requestor}}</p>
-                    </td>
-                    <td colspan="3" style="width: 49.61mm; height: 21.78mm; border: 1px solid black;">
+                    </div>
+                    <div style="width: 49.61mm; border-right: 1px solid black;">
                         <div class="d-flex flex-column justify-content-between text-center h-100">
                             <p>DISETUJUI</p>
                             <p class="m-0 mb-1" style="text-underline-offset: 8px"><u>{{$op->approved_by}}</u></p>
                         </div>
-                    </td>
-                    <td colspan="2" style="width: 44.21mm; height: 21.78mm; border: 1px solid black;">
+                    </div>
+                    <div style="width: 44.21mm;">
                         <div class="d-flex flex-column justify-content-between text-center h-100">
                             <p>DIMINTA</p>
                             <p class="m-0 mb-1" style="text-underline-offset: 8px"><u>{{$op->headOfSection->name}}</u>
                             </p>
                         </div>
-                    </td>
-                </tr>
-                </tfoot>
-            </table>
+                    </div>
+                </div>
+            </div>
 
             <div class="d-flex">
                 <a href="{{route('ops.index')}}" class="btn btn-default" style="width: fit-content;">Kembali</a>
 
-                <button class="btn btn-primary ml-auto" onclick="printTable()" style="width: fit-content;">
+                <button class="btn btn-primary ml-auto" onclick="printArea()" style="width: fit-content;">
                     <i class="fa fa-print"></i>
-                    Print Table
+                    Print
                 </button>
             </div>
         </div>
@@ -122,8 +163,8 @@
 
 @section('scripts')
     <script>
-        function printTable() {
-            var printContents = document.getElementById('printableTable').outerHTML;
+        function printArea() {
+            var printContents = document.getElementById('printableArea').innerHTML;
             var originalContents = document.body.innerHTML;
 
             document.body.innerHTML = printContents;
