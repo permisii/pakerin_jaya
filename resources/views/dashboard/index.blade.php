fc089b0073c715b10cb46ebde1a24fd0124c90a9
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-12-06 23:24:28 +0700

    update(rev): update dashboard page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="small-box bg-info">
                     <div class="inner">
                         <h3>{{$unprocessed_pps}}</h3>
-                        <p>Barang yang belum diproses</p>
+                        <p>PP yang belum diproses</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
@@ -19,47 +19,47 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-                        <p>Bounce Rate</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
+            {{--            <div class="col-lg-3 col-6">--}}
+            {{--                <!-- small box -->--}}
+            {{--                <div class="small-box bg-success">--}}
+            {{--                    <div class="inner">--}}
+            {{--                        <h3>53<sup style="font-size: 20px">%</sup></h3>--}}
+            {{--                        <p>Bounce Rate</p>--}}
+            {{--                    </div>--}}
+            {{--                    <div class="icon">--}}
+            {{--                        <i class="ion ion-stats-bars"></i>--}}
+            {{--                    </div>--}}
+            {{--                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3>{{$users_count}}</h3>
-                        <p>User Registrations</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
+            {{--            <div class="col-lg-3 col-6">--}}
+            {{--                <!-- small box -->--}}
+            {{--                <div class="small-box bg-warning">--}}
+            {{--                    <div class="inner">--}}
+            {{--                        <h3>{{$users_count}}</h3>--}}
+            {{--                        <p>User Registrations</p>--}}
+            {{--                    </div>--}}
+            {{--                    <div class="icon">--}}
+            {{--                        <i class="ion ion-person-add"></i>--}}
+            {{--                    </div>--}}
+            {{--                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-danger">
-                    <div class="inner">
-                        <h3>65</h3>
-                        <p>Unique Visitors</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
+            {{--            <div class="col-lg-3 col-6">--}}
+            {{--                <!-- small box -->--}}
+            {{--                <div class="small-box bg-danger">--}}
+            {{--                    <div class="inner">--}}
+            {{--                        <h3>65</h3>--}}
+            {{--                        <p>Unique Visitors</p>--}}
+            {{--                    </div>--}}
+            {{--                    <div class="icon">--}}
+            {{--                        <i class="ion ion-pie-graph"></i>--}}
+            {{--                    </div>--}}
+            {{--                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
             <!-- ./col -->
         </div>
         <!-- /.row -->
