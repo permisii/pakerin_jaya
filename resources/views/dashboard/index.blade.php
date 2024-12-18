98ceb201cb4935c0d17cf580a2cb77235dfa493c
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-12-18 12:26:43 +0700

    update(rev): add unprocessed work instruction to dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,24 +14,29 @@
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="{{route('pps.index')}}" class="small-box-footer">More info <i
-                            class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{route('pps.index')}}" class="small-box-footer">
+                        More info
+                        <i class="fas fa-arrow-circle-right"></i>
+                    </a>
                 </div>
             </div>
             <!-- ./col -->
-            {{--            <div class="col-lg-3 col-6">--}}
-            {{--                <!-- small box -->--}}
-            {{--                <div class="small-box bg-success">--}}
-            {{--                    <div class="inner">--}}
-            {{--                        <h3>53<sup style="font-size: 20px">%</sup></h3>--}}
-            {{--                        <p>Bounce Rate</p>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="icon">--}}
-            {{--                        <i class="ion ion-stats-bars"></i>--}}
-            {{--                    </div>--}}
-            {{--                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>--}}
-            {{--                </div>--}}
-            {{--            </div>--}}
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{$drafted_work_instructions}}</h3>
+                        <p>IK yang belum diproses</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{route('work-instructions.index')}}" class="small-box-footer">
+                        More info
+                        <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
             <!-- ./col -->
             {{--            <div class="col-lg-3 col-6">--}}
             {{--                <!-- small box -->--}}
